6ea38e8231bf116aa7d38f1b7d3071770f028f14
Laust <lafa16ac@student.cbs.dk>
2017-11-28 13:43:07 +0100

    adskillelse af kryptering og dekryptering

--- src/main/java/server/utility/Crypter.java
+++ src/main/java/server/utility/Crypter.java
@@ -1,5 +1,6 @@
 package server.utility;
 
+import com.google.gson.Gson;
 import server.config.Config;
 
 public class Crypter {
@@ -8,11 +9,10 @@ public class Crypter {
     //https://github.com/KyleBanks/XOREncryption/blob/master/Java%20(Android%20compatible)/XOREncryption.java
 
     /**
-     *
      * @param input
      * @return Input
      */
-        public static String encryptDecrypt(String input) {
+    public static String encrypt(String input) {
 
         if (Config.getEncryption()) {
             char[] key = {'J', 'M', 'F'};
@@ -21,7 +21,27 @@ public class Crypter {
             for (int i = 0; i < input.length(); i++) {
                 output.append((char) (input.charAt(i) ^ key[i % key.length]));
             }
-            return output.toString();
+
+            String encrypted = new Gson().toJson(output.toString());
+
+            return encrypted;
+        } else {
+            return input;
+        }
+    }
+
+    public String decrypt(String input) {
+        if (Config.getEncryption()) {
+            char[] key = {'J', 'M', 'F'};
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < input.length(); i++) {
+                output.append((char) (input.charAt(i) ^ key[i % key.length]));
+            }
+
+            String decrypted = output.toString();
+
+            return decrypted;
         } else {
             return input;
         }
