25fa71906193946427131f043984e25376e1a794
Laust <lafa16ac@student.cbs.dk>
2018-04-19 12:47:23 +0200

    change sql script to remove foreign keys and add userSex for later usage

--- src/main/java/server/providers/sql.sql
+++ src/main/java/server/providers/sql.sql
@@ -4,26 +4,23 @@ DROP TABLE IF EXISTS student_has_purchased;
 
 
 CREATE TABLE IF NOT EXISTS users (
-  `idUser` int(255) NOT NULL AUTO_INCREMENT UNIQUE,
-  `nameUser` varchar(100) NOT NULL,
-  `RFIDUSER` varchar(255) NOT NULL,
-  `userIsAdmin` tinyint(1) NOT NULL DEFAULT '0'
+  `idUser`      int(255)     NOT NULL AUTO_INCREMENT UNIQUE,
+  `nameUser`    varchar(100) NOT NULL,
+  `RFIDUSER`    varchar(255) NOT NULL,
+  `userIsAdmin` tinyint(1)   NOT NULL DEFAULT '0',
+  `userSex`     varchar(1)   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS products (
-  `idProduct` int(255) NOT NULL AUTO_INCREMENT UNIQUE,
-  `nameProduct` varchar(100) NOT NULL,
-  `priceProduct` int(11) NOT NULL,
-  `isActive` tinyint(1) NOT NULL DEFAULT '0'
+  `idProduct`    int(255)     NOT NULL AUTO_INCREMENT UNIQUE,
+  `nameProduct`  varchar(100) NOT NULL,
+  `priceProduct` int(11)      NOT NULL,
+  `isActive`     tinyint(1)   NOT NULL DEFAULT '0'
 );
 
 CREATE TABLE student_has_purchased (
-  `users_idUser` int(255) NOT NULL,
-  `users_RFIDUser` VARCHAR(255) NOT NULL,
+  `users_idUser`         int(255)     NOT NULL,
+  `users_RFIDUser`       VARCHAR(255) NOT NULL,
   `products_nameProduct` varchar(100) NOT NULL,
-  `amountBought` int(255) NOT NULL
+  `amountBought`         int(255)     NOT NULL
 );
-
-ALTER TABLE `student_has_purchased` ADD CONSTRAINT `student_has_purchased_fk0` FOREIGN KEY (`users_idUser`) REFERENCES `Users`(`idUser`);
-
-ALTER TABLE `student_has_purchased` ADD CONSTRAINT `student_has_purchased_fk1` FOREIGN KEY (`products_nameProduct`) REFERENCES `Products`(`nameProduct`);
